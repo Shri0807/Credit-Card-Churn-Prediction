f6e142256ca07eb6936f27e71eb060a5f3203843
Shri0807 <shrikumar.shankar4@gmail.com>
2025-02-23 14:28:45 +0530

    Add Logging in Data Ingestion Class

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,7 +7,6 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sqlalchemy import create_engine
 from src.utils.exception import CustomException
-from src.utils.logger import logging
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
@@ -21,7 +20,7 @@ class DataIngestion:
     Arguments:
         config: Config.yaml file read as dictionary from Airflow Pipeline
     """
-    def __init__(self, config):
+    def __init__(self, config, logger):
         """
         Initializes the DataIngestion class with the provided configuration.
 
@@ -31,6 +30,7 @@ class DataIngestion:
         self.df = pd.DataFrame()
         self.df_postgres = pd.DataFrame()
         self.config = config["data_ingestion"]
+        self.logger = logger
 
     def read_data_csv(self):
         """
@@ -38,10 +38,13 @@ class DataIngestion:
         The data is stored in a DataFrame and saved as a raw CSV file.
         """
         try:
+            self.logger.warning("Data Ingestion: Read Data from CSV: Start")
             self.df = pd.read_csv(self.config["csv"]["data_path"])
             os.makedirs(os.path.dirname(self.config["csv"]["raw_data_path"]), exist_ok=True)
             self.df.to_csv(self.config["csv"]["raw_data_path"], index=False, header=True)
+            self.logger.warning("Data Ingestion: Read Data from CSV: End")
         except Exception as e:
+            self.logger.error("Data Ingestion: Error in Read Data from CSV")
             raise CustomException(e, sys)
     
     def read_data_postgres(self):
@@ -52,13 +55,15 @@ class DataIngestion:
         If an error occurs during fetching, it prints an error message.
         """
         try:
+            self.logger.warning("Data Ingestion: Read Data from Postgres: Start")
             db_url = f'postgresql+psycopg2://{self.config["postgres"]["POSTGRES_USER"]}:{self.config["postgres"]["POSTGRES_PASSWORD"]}@{self.config["postgres"]["POSTGRES_HOST"]}:{self.config["postgres"]["POSTGRES_PORT"]}/{self.config["postgres"]["POSTGRES_DB"]}'
             engine = create_engine(db_url)
             query = f'SELECT * FROM {self.config["postgres"]["POSTGRES_TABLE_NAME"]}'
             with engine.connect() as connection:
                 self.df_postgres = pd.read_sql(query, connection)
-            
+            self.logger.warning("Data Ingestion: Read Data from Postgres: End")
         except Exception as e:
+            self.logger.error("Data Ingestion: Error in Read Data from Postgres")
             raise CustomException(e, sys)
     
     def preproc_data(self):
@@ -74,6 +79,7 @@ class DataIngestion:
         - Saves the preprocessed data as a CSV file.
         """
         try:
+            self.logger.warning("Data Ingestion: Preprocessing : Start")
             self.df_postgres.rename({"clientnum": "CLIENTNUM"}, axis=1, inplace=True)
 
             #Join Dataframes
@@ -84,7 +90,9 @@ class DataIngestion:
             self.df['churn_flag'] = self.df['churn_flag'].map({'Attrited Customer': 1, 'Existing Customer': 0})
             os.makedirs(os.path.dirname(self.config["csv"]["preproc_data_path"]), exist_ok=True)
             self.df.to_csv(self.config["csv"]["preproc_data_path"], index=False, header=True)
+            self.logger.warning("Data Ingestion: Preprocessing : End")
         except Exception as e:
+            self.logger.error("Data Ingestion: Error in Preprocessing")
             raise CustomException(e, sys)
         
     def split_data(self):
@@ -96,6 +104,7 @@ class DataIngestion:
         - Saves the training and testing datasets as CSV files.
         """
         try:
+            self.logger.warning("Data Ingestion: Split Data : Start")
             X = self.df.drop(columns=['churn_flag'])
             y = self.df['churn_flag'].copy()
 
@@ -106,7 +115,9 @@ class DataIngestion:
 
             train.to_csv(self.config["csv"]["train_data_path"], index=False, header=True)
             test.to_csv(self.config["csv"]["test_data_path"], index=False, header=True)
+            self.logger.warning("Data Ingestion: Split Data : End")
         except Exception as e:
+            self.logger.error("Data Ingestion: Error in Split Data")
             raise CustomException(e, sys)
     
     def prepare_data(self):
@@ -119,11 +130,14 @@ class DataIngestion:
         - Splits it into training and testing datasets
         """
         try:
+            self.logger.warning("Data Ingestion: Prepare Data: Start")
             self.read_data_csv()
             self.read_data_postgres()
             self.preproc_data()
             self.split_data()
+            self.logger.warning("Data Ingestion: Prepare Data: End")
         except Exception as e:
+            self.logger.error("Data Ingestion: Error in Prepare Data")
             raise CustomException(e, sys)
         
 
